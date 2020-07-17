d1702b2823f2a378b8af4b5fad613d91dbec5810
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-07-17 16:32:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,11 +15,5 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch(age) {
-    case "age >= 13 && age <= 19":
-      console.log("You are a teenager");
-      break;
-    default:
-      console.log("You have an age");
-  }
+
 }
