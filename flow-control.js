0015bedcd56a37d8b46fce0e01b355f88d46e74c
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-07-17 17:32:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,11 @@
 function basicTeenager(age) {
-  if (age < 19 && age > 13) {
-    console.log("You are a teenager!");
+  if (age <= 19 && age >= 13) {
+    return "You are a teenager!"
   }
 }
 
 function teenager(age) {
-  if (age < 19 && age > 13) {
+  if (age <= 19 && age >= 13) {
     console.log("You are a teenager!");
   } else {
     console.log("You are not a teeanger");
