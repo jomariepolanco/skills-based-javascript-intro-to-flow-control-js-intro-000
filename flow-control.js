e87792e11f01555052152c8e642d18898de2d179
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-07-17 16:32:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,32 +1,16 @@
 function basicTeenager(age) {
-  let result;
-  if (13 > age < 19) {
-    result = "You are a teenager!"
-  }
+
 }
 
 function teenager(age) {
-  if (13 < teenager < 19)
-  console.log("You are a teenager!");
-} else {
-  console.log("You are not a teenager");
-  }
+
 }
 
 function ageChecker(age) {
-  if (ageChecker > 13 && ageChecker < 19)
-  console.log("You are a teenager!");
 
-} else if (age <=12) {
-  console.log("You are a kid");
-} else if (age > 19) {
-  console.log("You are a grownup");
 }
 
 function ternaryTeenager(age) {
-  return age > 13 && age < 19 ? "You are a teenager" :
-  "You are not a teenager"
-
 
 }
 
@@ -38,5 +22,4 @@ function switchAge(age) {
     default:
       console.log("You have an age");
   }
-
 }
